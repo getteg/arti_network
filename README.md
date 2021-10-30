d22fcf7baaac2243cd40624b41c520bfe831f373
getteg <9166861666@mail.ru>
2021-10-30 15:14:11 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"social network of artisans and consumers for the independent exchange and sale of goods and food produced by them" 
---
+++ arti/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArtiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'arti'
